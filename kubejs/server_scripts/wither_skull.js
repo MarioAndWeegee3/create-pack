1860f939f99ff9a2d88b85c272340a097c5d01ad
MarioAndWeegee3 <marioandweegee3@gmail.com>
2023-02-06 23:49:45 -0500

    Added a new way to obtain Wither Skeleton Skulls; haunting Bone Blocks

---
+++ kubejs/server_scripts/wither_skull.js
@@ -0,0 +1,30 @@
+// This script adds an alternative way of obtaining a wither skeleton skull
+
+ServerEvents.recipes(event => {
+    event.custom({
+        type: 'create:haunting',
+        ingredients: [
+            {
+                item: 'minecraft:bone_block',
+            }
+        ],
+        results: [
+            {
+                item: 'minecraft:wither_skeleton_skull',
+                chance: 0.2,
+            },
+            {
+                item: 'minecraft:nether_star',
+                chance: 0.001,
+            },
+            {
+                item: 'minecraft:wither_rose',
+                chance: 0.7,
+            },
+            {
+                item: 'minecraft:bone_block',
+                chance: 0.8,
+            },
+        ],
+    })
+})
